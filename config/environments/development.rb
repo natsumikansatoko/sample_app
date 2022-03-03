d352ebbd3ca9b1aa52f90250a18d8628702ea73c
EC2 Default User <ec2-user@ip-172-31-39-146.ec2.internal>
2022-03-03 13:31:37 +0000

    [Update] 投稿機能を作ろう

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "8d2328da8d2e4470b94cb151714d81f5.vfs.cloud9.us-east-1.amazonaws.com"
 end
